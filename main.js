5b4dc6bb92de4aef4cb74d9e9fe9b5879b19d106
Esteban Blanco Ortuño <eb023024@gmail.com>
2023-07-09 21:29:14 +0200

    clase 7

--- main.js
+++ main.js
@@ -1,8 +1,50 @@
-const pepe = {
-    name: pepe3;
-    age: 33;
-    points: 100;
-    socialmedia: {
+class LearningPath {
 
-    };
 }
+
+const escuelaWeb = new LearningPath();
+const escuelaData = new LearningPath();
+
+class Student {
+  constructor({
+    name,
+    email,
+    username,
+    twitter = undefined,
+    instagram = undefined,
+    approvedCourses = [],
+    learningPaths = [],
+  }) {
+    this.name = name;
+    this.email = email;
+    this.username = username;
+    this.socialMedia = {
+      twitter,
+      instagram,
+    };
+    this.approvedCourses = approvedCourses;
+    this.learningPaths = learningPaths;
+  }
+}
+
+const esteban33 = new Student({
+  name: "esteban33",
+  username: "ebo010",
+  email: "ebo@gmail.com",
+  twitter: "ebo010twitter",
+  learningPaths: [
+    escuelaWeb,
+    escuelaData,
+  ],
+});
+
+const miguelito2 = new Student({
+  name: "Michelon6",
+  username: "michelon6",
+  email: "michel@hotmail.com",
+  instagram: "michelinsta",
+  learningPaths: [
+    escuelaWeb,
+    escuelaData,
+  ],
+});
