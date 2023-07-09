0ab3ec062aff20d5dc5f8a9244a033b387405f7f
Esteban Blanco Ortuño <eb023024@gmail.com>
2023-07-09 21:02:54 +0200

    rama7 main js

--- main.js
+++ main.js
@@ -1,55 +1,8 @@
-const natalia = {
-    name: "Natalia",
-    age: 20,
-    cursosAprobados: [
-        "Curso 1",
-        "Curso 2",
-    ],
-    aprobarCurso: function(nuevoCurso){
-        this.cursosAprobados.push(nuevoCurso)
-    }
-};
+const pepe = {
+    name: pepe3;
+    age: 33;
+    points: 100;
+    socialmedia: {
 
-natalia.cursosAprobados.push("Curso 3");
-
-function Student(name, age, cursosAprobados) {
-    this.name = name;
-    this.age = age;
-    this.cursosAprobados = cursosAprobados;
-//    this.aprobarCurso = function (nuevoCurso){
-//        this.cursosAprobados.push(nuevoCurso)
-    }
-    
-    Student.prototype.aprobarCurso = function (nuevoCurso){
-    this.cursosAprobados.push(nuevoCurso)
-    }
-
-const juanito = new Student(
-    "Juanito Perez",
-    45,
-    [
-        "curso 9"
-    ],
-)
-
-// prototipos con la sintaxis de clases
-
-class Student2 {
-    constructor(name, age, cursosAprobados){
-       this.name = name;
-       this.age = age;
-       this.cursosAprobados = cursosAprobados;
-    }
-    aprobarCurso(nuevoCurso) {
-        this.cursosAprobados.push(nuevoCurso)
-    }
+    };
 }
-
-const Miguel = new Student2(
-    "Miguel",
-    28,
-    [
-        "curso 18"
-    ],
-);
-
